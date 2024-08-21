80d0d3f40df1f8a5b034a12fc672382032cc178d
frederick <frederickros45@gmail.com>
2024-08-21 03:27:30 -0400

    Responsive hotfix

--- src/app/components/contact_cta.tsx
+++ src/app/components/contact_cta.tsx
@@ -3,8 +3,8 @@ import { BigBTN } from "./buttons";
 export function C_CTA(){
     return(
         <div className="flex flex-col w-full py-16 gap-16 px-5vw 2xl:px-0 items-center">    
-            <div className="flex justify-between w-full h-fit max-w-screen-xl 2xl:max-w-screen-3xl">
-                <p className="text-6xl font-bold max-w-screen-sm">¿Interesado en nuestros servicios?</p>
+            <div className="flex flex-col lg:flex-row justify-between gap-8 w-full h-fit max-w-screen-xl 2xl:max-w-screen-3xl">
+                <p className="text-5xl font-bold max-w-screen-sm">¿Interesado en nuestros servicios?</p>
                 <BigBTN label='Pongamonos en contacto' url='/contact'/>
             </div>
         </div>  
